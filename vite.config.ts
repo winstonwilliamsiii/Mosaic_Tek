12c8cf2b3deba0a4d3704fb91c5e24ce09f44383
BentleyBot <winstonwilliamsiii@gmail.com>
2026-09-13 17:47:09 -0400

    Updates to configs

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,10 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   tanstackStart: {
-    server: { entry: "server" },
+    // REMOVE server: { entry: "server" }
+  },
+  nitro: {
+    preset: "static"
   },
   vite: {
     build: {
@@ -18,8 +21,5 @@ export default defineConfig({
     resolve: {
       tsconfigPaths: true
     }
-  },
-  nitro: {
-    preset: "static"
   }
 });
